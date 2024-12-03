08dadc7111f8a053bf6ab211de0e0d8afeba04ae
Kabilan <kabilan10042000@gmail.com>
2024-12-03 23:22:33 +0530

    JSON Commit

---
+++ JSON.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> JavaScript object</h2>
+<p id="demo"></p>
+
+<script>
+const myObj = {name:"Giri", age:20, city:"Chennai"};
+document.getElementById("demo").innerHTML = myObj["name"];
+</script>
+
+</body>
+</html>
