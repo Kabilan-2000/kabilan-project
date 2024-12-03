671933dd879bb8eeaa9782087034c849142da8a9
Kabilan <kabilan10042000@gmail.com>
2024-12-03 08:35:10 +0530

    Config Commit

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+// database credentials
+$db_host      = 'localhost';
+$db_name      = 'dev_topbestsoftware';
+$db_user      = 'root';
+$db_user_pass = '';
+
+// base url, where the script will be installed
+// *please do not include a trailing slash*
+$baseurl = 'http://localhost/localtopbest';
+
+// include global logic
+require_once('common.inc.php');
