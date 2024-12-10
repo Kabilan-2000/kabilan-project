ba1e13ac8acf03a5cc1d322a3c6825d9e6d876d1
Kabilan <kabilan10042000@gmail.com>
2024-12-10 10:50:29 +0530

    test2 commit

--- JSON.php
+++ JSON.php
@@ -6,9 +6,101 @@
 <p id="demo"></p>
 
 <script>
-const myObj = {name:"Giri", age:20, city:"Chennai"};
-document.getElementById("demo").innerHTML = myObj["name"];
+const myObj ={
+  "success": true,
+  "data": {
+    "name": "SOS Bags",
+    "webproduct_id": 104,
+    "store_code": "shopify",
+    "type": "upload_personalization_configuration",
+    "product_type": "upload_personalization_configuration",
+    "quantity": "100",
+    "options": {
+      "quantity": {
+        "label": "Quantity",
+        "default_value": "quantity_100",
+        "type": "single",
+        "info": "",
+        "hidden": false,
+        "values": [
+          {
+            "label": "100",
+            "value": "quantity_100",
+            "image": null,
+            "attributes": {
+              "qty": "100",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          },
+          {
+            "label": "250",
+            "value": "quantity_250",
+            "image": null,
+            "attributes": {
+              "qty": "250",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          },
+          {
+            "label": "500",
+            "value": "quantity_500",
+            "image": null,
+            "attributes": {
+              "qty": "500",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          },
+          {
+            "label": "1000",
+            "value": "quantity_1000",
+            "image": null,
+            "attributes": {
+              "qty": "1000",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          },
+          {
+            "label": "2500",
+            "value": "quantity_2500",
+            "image": null,
+            "attributes": {
+              "qty": "2500",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          },
+          {
+            "label": "5000",
+            "value": "quantity_5000",
+            "image": null,
+            "attributes": {
+              "qty": "5000",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          },
+          {
+            "label": "10000",
+            "value": "quantity_10000",
+            "image": null,
+            "attributes": {
+              "qty": "10000",
+              "qty_setup_price": "0",
+              "qty_p1000": "0"
+            }
+          }
+        ]
+      }
+    }
+  }
+};
+console.log(myObj)
 </script>
+
 
 </body>
 </html>
