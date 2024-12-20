690430c705da298b7bccaef0d8f470cd686de88c
Kabilan <kabilan10042000@gmail.com>
2024-12-21 00:00:26 +0530

    Multidimensional Commit

---
+++ Multidimensional array.php
@@ -0,0 +1,15 @@
+<?php
+$products = array(
+    array("product" => "Laptop", "price" => 800, "stock" => 10),
+    array("product" => "Phone", "price" => 500, "stock" => 15),
+    array("product" => "Tablet", "price" => 300, "stock" => 20)
+);
+
+echo $products[0]["product"]; 
+echo $products[1]["price"]; 
+
+
+?>
+
+
+
