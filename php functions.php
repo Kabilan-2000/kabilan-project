bf4b8b20993188769e8f56dc0631e885ef560367
Kabilan <kabilan10042000@gmail.com>
2024-12-24 23:33:19 +0530

    PHP Functions Commit

---
+++ php functions.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function familyName($fname) {
+  echo "$fname Refsnes<br>";
+}
+
+familyName("Ravi");
+familyName("Kumar");
+familyName("Ragu");
+familyName("Siva");
+familyName("Babu");
+?>
+
+</body>
+</html>
