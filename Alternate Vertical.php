a05b7b295b6b41e6a989cd7a1817bd598173b58a
Kabilan <kabilan10042000@gmail.com>
2025-01-02 23:25:56 +0530

    Alternate Vertical Commit

---
+++ Alternate Vertical.php
@@ -0,0 +1,15 @@
+<?php
+$input = 8;
+for ($i = 8; $i >= 1; $i--) {
+    if ($i % 2 == 0) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+    } else {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "#";
+        }
+    }
+    echo "<br>";
+}
+?>
